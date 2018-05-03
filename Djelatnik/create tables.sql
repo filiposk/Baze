c3d1102f24192e35d824db2543e4cb7b71929b2c
DESKTOP-B2IB0NK\Zla Paprika <fpavlovic1995@gmail.com>
2018-05-03 20:31:44 +0200

    Dodavanje stranih kljuceva, maknuo not null s nadredeni

--- Djelatnik/create tables.sql
+++ Djelatnik/create tables.sql
@@ -8,6 +8,9 @@ CREATE TABlE Zaposlenik(
     Sifra int PRIMARY KEY NOT NULL auto_increment,
     Ime varchar(50) NOT NULL,
     Prezime varchar(50) NOT NULL,
-    Nadredeni int NOT NULL,
+    Nadredeni int,
     RadnoMjesto int NOT NULL
 );
+
+ALTER TABLE Zaposlenik ADD FOREIGN KEY(Nadredeni) references Zaposlenik(Sifra);
+ALTER TABLE Zaposlenik ADD FOREIGN KEY(RadnoMjesto) references RadnoMjesto(Sifra);
